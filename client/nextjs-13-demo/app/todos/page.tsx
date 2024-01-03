d0b6d16f6bd4225122e8987ba56584a5651d677a
Santhosh_Pemmaka <30383739+santhoshpemmaka@users.noreply.github.com>
2024-01-04 00:22:40 +0530

    update styles file

--- client/nextjs-13-demo/app/todos/page.tsx
+++ client/nextjs-13-demo/app/todos/page.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import TodoList from './TodoList';
-import styles from './TodoList.module.css';
+import _ from './TodoList.module.css';
 const Todos = () => {
   return (
-      <div className={styles.todoListHeader}>
+      <div className={_.todoListHeader}>
           <h1>This is where the todos will be listed....</h1>
       </div>
   )
